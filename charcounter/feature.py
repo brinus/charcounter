e8ce0afe16824fb66211399c3dd55950522d6497
brinus <brinimatteo@gmail.com>
2021-09-28 18:04:35 +0200

    minors

--- charcounter/feature.py
+++ charcounter/feature.py
@@ -1,6 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file_name', help='The file to be analized must be in plain text format (.txt), it can be placed in /test folder or called with the absolute path.')
+parser.add_argument('file_name', help='The file to be analized must be in plain text format (.txt), it can be placed in /test folder or called with its absolute path.')
 args = parser.parse_args()
 file_name = args.file_name
