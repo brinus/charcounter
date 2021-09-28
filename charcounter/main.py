4573f3448094c2a970c2135cdca6598e3320df98
brinus <brinimatteo@gmail.com>
2021-09-28 18:01:38 +0200

    Added new files for tests and cmd line input, modified main file

--- charcounter/main.py
+++ charcounter/main.py
@@ -1,12 +1,45 @@
 import os
 import sys
+import feature
+
+char_dict = {
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 0,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 0,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 0,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 0,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0
+}
 
 def count(str_):
-    pass
+    for char in str_:
+        if char in char_dict or char.lower() in char_dict:
+            char_dict[char.lower()] += 1
 
 if __name__ == '__main__':
-    os.chdir(sys.path[0]+'/..')
-    file_name = str(input())
-    with open(file_name, 'r') as file:
+    os.chdir(sys.path[0]+'/../test')
+    with open(feature.file_name, 'r') as file:
         text = file.read()
-    print(text)
+
+count(text)
+print(char_dict)
