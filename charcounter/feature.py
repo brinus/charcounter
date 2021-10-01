e4bc02482b69785ade476c2467475dd10ae172a6
brinus <brinimatteo@gmail.com>
2021-10-01 10:28:32 +0200

    Improved comments for new argparse feature about histogram plot option

--- charcounter/feature.py
+++ charcounter/feature.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file_name', help='The file to be analized must be in plain text format (.txt), it can be placed in /test folder or called with its absolute path.')
-parser.add_argument('-p', '--plothist', nargs='?', default=None, const='alphabetic', choices=['alphabetic', 'decreasing'], help='The user can specify if the histogram bar must be ordered in values-decreasing order or alphabetic-keys order.', metavar=('ORDERING'))
+parser.add_argument('-p', '--plothist', nargs='?', default=None, const='alphabetic', choices=['alphabetic', 'decreasing'], help='[ORDERING] = [alphabetic, decreasing] \nThe user can specify if the histogram bar must be ordered in values-decreasing order or alphabetic-keys order. If nothing is passed to -p/--plothist, the default choice is alphabetic ordering.', metavar=('ORDERING'))
 args = parser.parse_args()
 file_name = args.file_name
 plot_type = args.plothist
