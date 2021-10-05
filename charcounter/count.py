413d774583706abff78f8ab4ac1b82da0868250f
brinus <brinimatteo@gmail.com>
2021-10-05 18:29:34 +0200

    Added setting() function, improved main part for choosing if evaluate stats or not

--- charcounter/count.py
+++ charcounter/count.py
@@ -9,93 +9,94 @@ import logging
 import matplotlib.pyplot as plt
 
 char_dict = {
-    'a': 0,
-    'b': 0,
-    'c': 0,
-    'd': 0,
-    'e': 0,
-    'f': 0,
-    'g': 0,
-    'h': 0,
-    'i': 0,
-    'j': 0,
-    'k': 0,
-    'l': 0,
-    'm': 0,
-    'n': 0,
-    'o': 0,
-    'p': 0,
-    'q': 0,
-    'r': 0,
-    's': 0,
-    't': 0,
-    'u': 0,
-    'v': 0,
-    'w': 0,
-    'x': 0,
-    'y': 0,
-    'z': 0
+	'a': 0,
+	'b': 0,
+	'c': 0,
+	'd': 0,
+	'e': 0,
+	'f': 0,
+	'g': 0,
+	'h': 0,
+	'i': 0,
+	'j': 0,
+	'k': 0,
+	'l': 0,
+	'm': 0,
+	'n': 0,
+	'o': 0,
+	'p': 0,
+	'q': 0,
+	'r': 0,
+	's': 0,
+	't': 0,
+	'u': 0,
+	'v': 0,
+	'w': 0,
+	'x': 0,
+	'y': 0,
+	'z': 0
 }
 
-def count():
-    ''' Function that given a file in input, counts the numer
-    '''
-    os.chdir(sys.path[0]+'/../test')
-    ''' Placing the working directory in ~/charcounter/test/ so that files are  \
-        organized in a different directory to mantain the project as clean as   \
-        possible.
-    '''
-    try:
-        with open(feature.file_name, 'r', encoding='utf-8') as file:
-            text = file.read()    
-    except IOError:
-        logging.error('File not found, check file name or absolute path if provided')
-        return  
-    
-    for char in text:
-        if char.lower() in char_dict:
-            char_dict[char.lower()] += 1
-    
-    print(f'Time elapsed = {(time()-start):.6f} second(s)')
-    print(char_dict)
-    return
-    
+def settings():
+	os.chdir(sys.path[0]+'/../test')
+	try:
+		with open(feature.file_name, 'r', encoding='utf-8') as file:
+			text = file.read()    
+	except IOError:
+		logging.error('File not found, check file name or absolute path if provided')
+		return None
+	return text
+
+def count(str_):
+	n_characters = 0
+	for char in str_:
+		n_characters += 1
+		if char.lower() in char_dict:
+			char_dict[char.lower()] += 1
+	for key in char_dict:
+		print(f'{key} : {(char_dict[key]/n_characters):.5f}')
+	return
+	
 def count_with_stats(str_):
-    n_words=0
-    n_characters_no_spaces=0
-    n_characters=0
-    n_letters=0
-    '''Function for doing stats on texts, it returns the number of characters, number of words, number of letters'''
-    for char in text:
-        n_characters+=1
-        if char!='\n':
-            n_characters_no_spaces+=1
-        if char.lower() in char_dict:
-            n_letters+=1
-    print('Stats of the text:\n')
-    print(f'Number of characters with spaces: {n_characters}\nNumber of characters without spaces: {n_characters_no_spaces}\nNumber of letters: {n_letters}\n')
+	# n_words=0
+	n_characters_no_spaces=0
+	n_characters=0
+	n_letters=0
+	'''Function for doing stats on texts, it returns the number of characters, number of words, number of letters'''
+	for char in str_:
+		n_characters += 1
+		if char != '\n':
+			n_characters_no_spaces += 1
+		if char.lower() in char_dict:
+			n_letters += 1
+			char_dict[char.lower()] += 1
+	for key in char_dict:
+		print(f'{key} : {(char_dict[key]/n_characters):.5f}')
+	print(f'''Stats of the text:
+	Number of characters with spaces: {n_characters}
+	Number of characters without spaces: {n_characters_no_spaces}
+	Number of letters: {n_letters}
+''')
 
 def hist_plot():
-    if feature.plot_type is not None:
-        _, ax = plt.subplots(1,1)
-        if feature.plot_type == 'alphabetic':
-            ax.bar(char_dict.keys(), char_dict.values())
-        if feature.plot_type == 'decreasing':
-            char_dict_sorted = {k: v for k, v in sorted(char_dict.items(), key=lambda item: item[1], reverse=True)}
-            ax.bar(char_dict_sorted.keys(), char_dict_sorted.values())
-        plt.show()
-    return
+	if feature.plot_type is not None:
+		_, ax = plt.subplots(1,1)
+		if feature.plot_type == 'alphabetic':
+			ax.bar(char_dict.keys(), char_dict.values())
+		if feature.plot_type == 'decreasing':
+			char_dict_sorted = {k: v for k, v in sorted(char_dict.items(), key=lambda item: item[1], reverse=True)}
+			ax.bar(char_dict_sorted.keys(), char_dict_sorted.values())
+		plt.show()
+	return
 
 if __name__ == '__main__':
-    start = time()
-    os.chdir(sys.path[0]+'/../test')
-    with open(feature.file_name, 'r', encoding='utf-8') as file:
-        text = file.read()
-    count()
-    if feature.stats:
-        count_with_stats(text)
-        print(f'Time elapsed = {(time()-start):.6f} second(s)')
-    else:
-        print(f'Time elapsed = {(time()-start):.6f} second(s)')
-    
-    hist_plot()
+	start = time()
+	text = settings()
+	if text is not None:
+		if feature.stats:
+			count_with_stats(text)
+			print(f'Time elapsed = {(time()-start):.6f} second(s)')
+		else:
+			count(text)
+			print(f'Time elapsed = {(time()-start):.6f} second(s)')
+		hist_plot()
--- charcounter/feature.py
+++ charcounter/feature.py
@@ -1,20 +1,20 @@
 ''' This module provides implementations for command line options, argparse     \
-    library is used in order to achieve that. See argparse documentations at:   \
-        https://docs.python.org/3/library/argparse.html    
+	library is used in order to achieve that. See argparse documentations at:   \
+		https://docs.python.org/3/library/argparse.html    
 '''
 
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file_name', help='The file to be analized must be in plain \
-    text format (.txt), it can be placed in /test folder or called with its abso\
-        lute path.')
-parser.add_argument('-p', '--plothist', nargs='?', default=None, const='alphabet\
-    ic', choices=['alphabetic', 'decreasing'], help='[ORDERING] = [alphabetic, d\
-    ecreasing] \nEnables histogram plot. The user can specify if the histogram b\
-    ar must be ordered in values-decreasing order or alphabetic-keys order. If n\
-    othing is passed to -p/--plothist, the default choice is alphabetic ordering\
-    .', metavar=('ORDERING'))
+	text format (.txt), it can be placed in /test folder or called with its abso\
+		lute path.')
+parser.add_argument('-p', '--plothist', nargs='?', default=None, 
+	const='alphabetic', choices=['alphabetic', 'decreasing'], help='[ORDERING] =\
+	[alphabetic, decreasing] \nEnables histogram plot. The user can specify if t\
+	he histogram bar must be ordered in values-decreasing order or alphabetic-ke\
+	ys order. If nothing is passed to -p/--plothist, the default choice is alpha\
+	betic ordering.', metavar=('ORDERING'))
 parser.add_argument("-s","--stats",help='Prints on terminal the stats of the book',action="store_true")
 
 args = parser.parse_args()
